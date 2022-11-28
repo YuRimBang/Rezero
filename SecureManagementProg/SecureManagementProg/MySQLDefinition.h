24464ae71bb35b734a71f2c91acbae789a5f3112
jiyul0204 <87352553+jiyul0204@users.noreply.github.com>
2022-11-28 15:30:42 +0900

    커밋 5차
    - 재배포 패키지 추가
    - Additional 폴더 추가

--- SecureManagementProg/SecureManagementProg/MySQLDefinition.h
+++ SecureManagementProg/SecureManagementProg/MySQLDefinition.h
@@ -5,8 +5,10 @@
 
 //#define		DB_PORT					3306	
 //#define		DB_HOST				"localhost"
-#define		DB_USER					"root"
-#define		DB_PASSWORD		"P@ssw0rd!"
+//#define		DB_PASSWORD		"P@ssw0rd!"
+
+#define		DB_USER					"rezero"
+#define		DB_PASSWORD		"password1234"
 #define		DB_NAME			"secureprogmanagerdb"
 
 #define		DB_PORT					3306	
